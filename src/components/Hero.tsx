92b337d8c0bb92937eaf60f9a8962df562e27194
Jose Manaure <jmanaure87@gmail.com>
2025-11-06 16:30:04 -0300

    frontend completo

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -45,7 +45,7 @@ export default function Hero() {
   const BASE_URL =
     process.env.NODE_ENV === "production"
       ? process.env.NEXT_PUBLIC_API_URL || "https://tu-backend-production-url.com"
-      : "https://beige-days-greet.loca.lt"; // 👈 tu backend local (Railway o Express local)
+      : "https://wide-files-send.loca.lt"; // 👈 tu backend local (Railway o Express local)
 
   useEffect(() => {
     if (messages.length === 0) {
